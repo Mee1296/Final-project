7c910f134fbfe6a4f2b2d89e5cf219e84b963e92
Ittichet Thongsang <zagif2151382@gmail.com>
2024-04-18 17:56:43 +0700

    Index and main created

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -1,23 +1,49 @@
-import { handleCreateMember, populateMembers } from "./member.js";
-import { fetchAndDrawTable, handleCreateItem, handleFilterItem } from "./table.js";
+// import '../style.css'
+//import Phaser from 'phaser';
 
-document.addEventListener("DOMContentLoaded", () => {
-  fetchAndDrawTable();
+const sizes = {
+  width:960,
+  height:540
+}
 
-  populateMembers();
 
-  const addItemButton = document.getElementById("add-newrow");
-  addItemButton.addEventListener("click", () => {
-    handleCreateItem();
-  });
 
-  const filterButton = document.getElementById("filter-button");
-  filterButton.addEventListener("click", () => {
-    handleFilterItem();
-  });
 
-  const addMemberButton = document.getElementById("add-member");
-  addMemberButton.addEventListener("click", () => {
-    handleCreateMember();
-  });
-});
+const config = {
+  type: Phaser.Auto,
+  width:sizes.width,
+  height:sizes.height,
+  canvas:gameCanvas,
+}
+
+const game = new Phaser.Game(config)
+
+class GameScene extends Phaser.Scene{
+  constructor(){
+    super("scene-game")
+    this.player
+  }
+
+  preload(){
+    this.load.image("bg", "./assets/space.png")
+  }
+
+  create(){
+    const bg = this.add.image(0,0,"bg").setOrigin(0,0)
+    
+
+  }
+
+  update(){
+
+  }
+
+}
+
+// Add the scene to the game
+game.scene.add('gameScene', GameScene);
+
+// Start the "myScene" scene
+game.scene.start('gameScene');
+
+
