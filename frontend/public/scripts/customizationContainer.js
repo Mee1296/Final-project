36a57bc42f21259661630b3ed8d2fc089e8efdd5
Ittichet Thongsang <zagif2151382@gmail.com>
2024-04-21 14:23:59 +0700

    customization finished

--- frontend/public/scripts/customizationContainer.js
+++ frontend/public/scripts/customizationContainer.js
@@ -2,7 +2,8 @@ const sizes = {
     width:960,
     height:540
   }
-  
+
+const characterRatio = 0.6
 
 class CustomizationContainer extends Phaser.GameObjects.Container {
     constructor(scene, x, y) {
@@ -16,6 +17,8 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       const leftArrowX = 25
       const rightArrowX = leftArrowX+210
       const arrowGap = 45
+      
+      
   
       const colorArray = [0xffffff,0x181818,0xB8B8B8,0xff0000,0xff8d00,0xffdf00,
         0x02ff00,0x008000,0x00d9ff,0x006dff,0x7e47ff,0xef7aff,0xb55700]
@@ -27,7 +30,16 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       const rect = this.scene.add.rectangle(0,0,sizes.width/3+10,sizes.height,0xA79277,1).setOrigin(0,0)
       const exit = this.scene.add.image(sizes.width/3-20,30,'exit').setScale(0.2)
       const heading = this.scene.add.bitmapText(100,30,'mono','CUSTOMIZE',20)
-      const displayRect = this.scene.add.rectangle(74,60,158,320,0xffffff,1).setOrigin(0,0)
+      const displayRect = this.scene.add.rectangle(74,60,158,320,0x000000,1).setOrigin(0,0)
+
+
+      const characterDisplay = this.scene.add.image(74,60,'baseplayer').setOrigin(0,0).setScale(characterRatio).setTint(this.scene.skinTint)
+      const characterHairDisplay = this.scene.add.sprite(74,60,'hairpiece',this.scene.hairIndex).setOrigin(0,0).setScale(characterRatio).setTint(hairColorArray.at(this.scene.hairColor))
+      const characterShirtDisplay = this.scene.add.sprite(74,60,'shirt',this.scene.shirtIndex).setOrigin(0,0).setScale(characterRatio).setTint(colorArray.at(this.scene.shirtColor))
+      const characterPantsDisplay = this.scene.add.sprite(74,60,'pants',this.scene.pantsIndex).setOrigin(0,0).setScale(characterRatio).setTint(colorArray.at(this.scene.pantsColor))
+      const characterAccDisplay = this.scene.add.sprite(74,60,'acc',this.scene.accIndex).setOrigin(0,0).setScale(characterRatio)
+      const characterEyeDisplay = this.scene.add.sprite(74,60,'eyes',0).setOrigin(0,0).setScale(characterRatio).setTint(this.scene.eyeTint)
+      const characterShoeDisplay = this.scene.add.sprite(74,60,"shoes",0).setOrigin(0,0).setScale(characterRatio).setAlpha(this.scene.shoeAlpha).setTint(this.scene.shoeTint)
   
       //adding arrows
       const leftarrow1 =  this.scene.add.image(leftArrowX,arrowY,'leftarrow').setOrigin(0,0).setScale(0.17).setTint(0x08ff00)
@@ -54,14 +66,6 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       const skin4 = this.scene.add.rectangle(247,402.5,35,35,0xbf7848,1).setOrigin(0,0)
       const skin5 = this.scene.add.rectangle(285,402.5,35,35,0x985d37,1).setOrigin(0,0)
   
-      //adding haircolor
-    //   const hairColor = this.scene.add.bitmapText(10,410,'mono','Hair Color',20).setOrigin(0,0)
-    //   const blackBox2 = this.scene.add.rectangle(130,400,193,40,0x000000,1).setOrigin(0,0)
-    //   const hair1 = this.scene.add.rectangle(133,402.5,35,35,0x3d3d3d,1).setOrigin(0,0)
-    //   const hair2 = this.scene.add.rectangle(171,402.5,35,35,0x593722,1).setOrigin(0,0)
-    //   const hair3 = this.scene.add.rectangle(209,402.5,35,35,0xfcba03,1).setOrigin(0,0)
-    //   const hair4 = this.scene.add.rectangle(247,402.5,35,35,0xE17D17,1).setOrigin(0,0)
-    //   const hair5 = this.scene.add.image(285,402.5,'none').setOrigin(0,0).setScale(0.136)
       
       //adding eyeclor
       const eyeColor = this.scene.add.bitmapText(10,450,'mono','Eye Color',20).setOrigin(0,0)
@@ -85,10 +89,19 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
   
       
   
-      container.add(rect);
+      container.add(rect)
       container.add(exit)
       container.add(heading)
       container.add(displayRect)
+
+      container.add(characterDisplay)
+      container.add(characterEyeDisplay)
+      container.add(characterShirtDisplay)
+      container.add(characterAccDisplay)
+      container.add(characterHairDisplay)
+      container.add(characterPantsDisplay)
+      container.add(characterShoeDisplay)
+      
   
       container.add(leftarrow1)
       container.add(rightarrow1)
@@ -113,13 +126,6 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       container.add(skin4)
       container.add(skin5)
   
-    //   //container.add(hairColor)
-    //   //container.add(blackBox2)
-    //   container.add(hair1)
-    //   container.add(hair2)
-    //   container.add(hair3)
-    //   container.add(hair4)
-    //   container.add(hair5)
   
       container.add(eyeColor)
       container.add(blackBox3)
@@ -149,6 +155,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
         }else{
           this.scene.hairIndex -= 3
         }
+        characterHairDisplay.setFrame(this.scene.hairIndex)
       });
       rightarrow1.setInteractive();
       rightarrow1.on('pointerup', () => { 
@@ -158,6 +165,9 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
         }else{
           this.scene.hairIndex +=3 
         }
+
+        //change display
+        characterHairDisplay.setFrame(this.scene.hairIndex)
       });
   
       //hair color changer
@@ -170,6 +180,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.hairColor--
         }
         this.scene.hair.setTint(hairColorArray.at(this.scene.hairColor))
+        characterHairDisplay.setTint(hairColorArray.at(this.scene.hairColor))
       });
       rightarrow2.setInteractive();
       rightarrow2.on('pointerup', () => { 
@@ -181,13 +192,14 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           }
           console.log(hairColorArray.at(this.scene.hairColor))
           this.scene.hair.setTint(hairColorArray.at(this.scene.hairColor))
+          characterHairDisplay.setTint(hairColorArray.at(this.scene.hairColor))
       });
   
       //top changer
       leftarrow3.setInteractive();
       leftarrow3.on('pointerup', () => { 
           this.scene.pop.play()
-        console.log('Left Arrow3 Pressed!')
+        //console.log('Left Arrow3 Pressed!')
         if(this.scene.shirtIndex-9<0){
           this.scene.shirtIndex = 27
         }else{
@@ -200,11 +212,12 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
             this.scene.shirt.setAlpha(1)
         }
         this.scene.shirt.setFrame(this.scene.shirtIndex)
+        characterShirtDisplay.setFrame(this.scene.shirtIndex)
       });
       rightarrow3.setInteractive();
       rightarrow3.on('pointerup', () => { 
         this.scene.pop.play()
-        console.log('Right Arrow3 Pressed!')
+        //console.log('Right Arrow3 Pressed!')
         if(this.scene.shirtIndex+9>35){
           this.scene.shirtIndex = 0
         }else{
@@ -217,6 +230,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
             this.scene.shirt.setAlpha(1)
         }
         this.scene.shirt.setFrame(this.scene.shirtIndex)
+        characterShirtDisplay.setFrame(this.scene.shirtIndex)
       });
   
       //top color changer
@@ -229,6 +243,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.shirtColor--
         }
         this.scene.shirt.setTint(colorArray.at(this.scene.shirtColor))
+        characterShirtDisplay.setTint(colorArray.at(this.scene.shirtColor))
   
       });
       rightarrow4.setInteractive();
@@ -240,6 +255,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.shirtColor++
         }
         this.scene.shirt.setTint(colorArray.at(this.scene.shirtColor))
+        characterShirtDisplay.setTint(colorArray.at(this.scene.shirtColor))
       });
   
       //pants changer
@@ -252,6 +268,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.pantsIndex -= 9
         }
         this.scene.pants.setFrame(this.scene.pantsIndex)
+        characterPantsDisplay.setFrame(this.scene.pantsIndex)
       });
       rightarrow5.setInteractive();
       rightarrow5.on('pointerup', () => { 
@@ -262,6 +279,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.pantsIndex += 9
         }
         this.scene.pants.setFrame(this.scene.pantsIndex)
+        characterPantsDisplay.setFrame(this.scene.pantsIndex)
       });
   
       //pants color changer
@@ -274,6 +292,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.pantsColor--
         }
         this.scene.pants.setTint(colorArray.at(this.scene.pantsColor))
+        characterPantsDisplay.setTint(colorArray.at(this.scene.pantsColor))
       });
       rightarrow6.setInteractive();
       rightarrow6.on('pointerup', () => { 
@@ -284,6 +303,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
           this.scene.pantsColor++
         }
         this.scene.pants.setTint(colorArray.at(this.scene.pantsColor))
+        characterPantsDisplay.setTint(colorArray.at(this.scene.pantsColor))
       });
 
       //acc selector
@@ -295,6 +315,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
         }else{
           this.scene.accIndex -= 3
         }
+        characterAccDisplay.setFrame(this.scene.accIndex)
       });
       rightarrow7.setInteractive();
       rightarrow7.on('pointerup', () => { 
@@ -304,6 +325,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
         }else{
           this.scene.accIndex +=3 
         }
+        characterAccDisplay.setFrame(this.scene.accIndex)
       });
   
       //skin color
@@ -311,65 +333,42 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       skin1.setInteractive()
       skin1.on('pointerup',() => {
         this.scene.player.setTint(0xffffff)
+        characterDisplay.setTint(0xffffff)
+        this.scene.skinTint = 0xffffff
         ,this.scene.pop.play()
       })
   
       skin2.setInteractive()
       skin2.on('pointerup',() => {
         this.scene.player.setTint(0xFfda9f)
+        characterDisplay.setTint(0xFfda9f)
+        this.scene.skinTint = 0xFfda9f
         ,this.scene.pop.play()
       })
   
       skin3.setInteractive()
       skin3.on('pointerup',() => {
         this.scene.player.setTint(0xe1bd8e)
+        characterDisplay.setTint(0xe1bd8e)
+        this.scene.skinTint = 0xe1bd84
         ,this.scene.pop.play()
       })
   
       skin4.setInteractive()
       skin4.on('pointerup',() => {
         this.scene.player.setTint(0xBf8b68)
+        characterDisplay.setTint(0xBf8b68)
+        this.scene.skinTint = 0xBf8b68
         ,this.scene.pop.play()
       })
   
       skin5.setInteractive()
       skin5.on('pointerup',() => {
         this.scene.player.setTint(0x986c4f)
+        characterDisplay.setTint(0x986c4f)
+        this.scene.skinTint = 0x986c4f
         ,this.scene.pop.play()
       })
-  
-    //   //hair color
-    //   hair1.setInteractive()
-    //   hair1.on('pointerup',() => {
-    //     this.scene.hair.setTint(0x3d3d3d)
-    //     ,this.scene.pop.play()
-    //   })
-  
-    //   hair2.setInteractive()
-    //   hair2.on('pointerup',() => {
-    //     this.scene.hair.setTint(0x593722)
-    //     ,this.scene.pop.play()
-    //   })
-  
-    //   hair3.setInteractive()
-    //   hair3.on('pointerup',() => {
-    //     this.scene.hair.setTint(0xfcba03)
-    //     ,this.scene.pop.play()
-    //   })
-  
-    //   hair4.setInteractive()
-    //   hair4.on('pointerup',() => {
-    //     this.scene.hair.setTint(0xE17D17)
-    //     ,this.scene.pop.play()
-    //   })
-  
-    //   hair5.setInteractive()
-    //   hair5.on('pointerup',() => {
-    //     this.scene.hair.setTint(0xffffff)
-    //     ,this.scene.pop.play()
-    //   })
-  
-  
   
   
       //eye color
@@ -377,30 +376,40 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       eye1.setInteractive()
       eye1.on('pointerup',() => {
         this.scene.eyes.setTint(0x181818)
+        characterEyeDisplay.setTint(0x181818)
+        this.scene.eyeTint = 0x1818181
         ,this.scene.pop.play()
       })
   
       eye2.setInteractive()
       eye2.on('pointerup',() => {
         this.scene.eyes.setTint(0x593722)
+        characterEyeDisplay.setTint(0x593722)
+        this.scene.eyeTint = 0x593722
         ,this.scene.pop.play()
       })
   
       eye3.setInteractive()
       eye3.on('pointerup',() => {
         this.scene.eyes.setTint(0x79a3dd)
+        characterEyeDisplay.setTint(0x79a3dd)
+        this.scene.eyeTint = 0x79a3dd
         ,this.scene.pop.play()
       })
   
       eye4.setInteractive()
       eye4.on('pointerup',() => {
         this.scene.eyes.setTint(0xA58454)
+        characterEyeDisplay.setTint(0xA58454)
+        this.scene.eyeTint = 0xA58454
         ,this.scene.pop.play()
       })
   
       eye5.setInteractive()
       eye5.on('pointerup',() => {
         this.scene.eyes.setTint(0x568A62)
+        characterEyeDisplay.setTint(0x568A62)
+        this.scene.eyeTint = 0x568a62
         ,this.scene.pop.play()
       })
   
@@ -411,6 +420,9 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       shoe1.on('pointerup',() => {
         this.scene.shoes.setAlpha(1),
         this.scene.shoes.setTint(0xffffff),
+        characterShoeDisplay.setAlpha(1).setTint(0xffffff)
+        this.scene.shoeAlpha = 1
+        this.scene.shoeTint = 0xffffff
         this.scene.pop.play()
       })
   
@@ -418,6 +430,9 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       shoe2.on('pointerup',() => {
         this.scene.shoes.setAlpha(1)
         this.scene.shoes.setTint(0x3d3d3d),
+        characterShoeDisplay.setAlpha(1).setTint(0x3d3d3d)
+        this.scene.shoeAlpha = 1
+        this.scene.shoeTint = 0x3d3d3d
         this.scene.pop.play()
       })
   
@@ -425,6 +440,9 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       shoe3.on('pointerup',() => {
         this.scene.shoes.setAlpha(1),
         this.scene.shoes.setTint(0xff0000),
+        characterShoeDisplay.setAlpha(1).setTint(0xff0000)
+        this.scene.shoeAlpha = 1
+        this.scene.shoeTint = 0xff0000
         this.scene.pop.play()
       })
   
@@ -432,12 +450,17 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
       shoe4.on('pointerup',() => {
         this.scene.shoes.setAlpha(1),
         this.scene.shoes.setTint(0xc45f00)
+        characterShoeDisplay.setAlpha(1).setTint(0xc45f00)
+        this.scene.shoeAlpha = 1
+        this.scene.shoeTint = 0xc45f00
         ,this.scene.pop.play()
       })
   
       shoe5.setInteractive()
       shoe5.on('pointerup',() => {
         this.scene.shoes.setAlpha(0),
+        characterShoeDisplay.setAlpha(0)
+        this.scene.shoeAlpha = 0
         this.scene.pop.play()
       })
   
@@ -453,4 +476,7 @@ class CustomizationContainer extends Phaser.GameObjects.Container {
     }
   }
 
-export default CustomizationContainer;
+
+  
+
+export default CustomizationContainer
