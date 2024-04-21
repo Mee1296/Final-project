93698e1b3ef682510601c1ec52ff99e38b7de0e4
Ittichet Thongsang <zagif2151382@gmail.com>
2024-04-21 18:59:20 +0700

    minor animation fixes

--- frontend/public/scripts/config.js
+++ frontend/public/scripts/config.js
@@ -3,8 +3,18 @@ export const BACKEND_URL = "http://localhost:3222";
 
 // TODO0: change element in MEMBERS to your group members.
 // WARNING: the _id parameters is required.
-export const MEMBERS = [
-  { name: "อติวงศ์ สุชาโต", _id: 1 },
-  { name: "โปรดปราน บุณยพุกกณะ", _id: 2 },
-  { name: "ณัฐวุฒิ หนูไฟโรจน์", _id: 3 },
-];
+export const config = {
+  type: Phaser.WEBGL,
+  width:sizes.width,
+  height:sizes.height,
+  canvas:gameCanvas,
+  physics:{
+    default:"arcade",
+    arcade:{
+      gravity:{y:speedDown},
+      debug:true
+    }
+
+  },
+  scene:[GameScene]
+}
