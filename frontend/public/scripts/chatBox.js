8b74fe6276ba049559bbe0bb7de771552cc858c3
Mee1296 <weeraphat61100@gmail.com>
2024-04-21 23:04:03 +0700

    textbox still not work

--- frontend/public/scripts/chatBox.js
+++ frontend/public/scripts/chatBox.js
@@ -28,6 +28,7 @@ class chatBox extends Phaser.GameObjects.Container {
         const rect = this.scene.add.rectangle(0,0,size.width/3+10,size.height,0xA79277,1).setOrigin(0,0)
         const exit = this.scene.add.image(size.width/3-20,30,'exit').setScale(0.2)
         const heading = this.scene.add.bitmapText(80,30,'mono','Chatbox',40)
+        
         // const displayRect = this.scene.add.rectangle(10,490,250,40,0xffffff,1).setOrigin(0,0)
 
         container.add(rect);
